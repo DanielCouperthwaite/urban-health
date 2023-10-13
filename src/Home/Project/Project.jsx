541b1031d6d6adb7eebcba1fbed5db9b16bca7b8
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2023-10-13 10:05:36 +0100

    add first screenshots to assets folder

--- src/Home/Project/Project.jsx
+++ src/Home/Project/Project.jsx
@@ -1,20 +1,7 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import './Project.css'
 
-// export const projectsArray = [
-//     {
-//         name: "Number Blast", 
-//         descr1: "Welcome to number Blast, a reaction based number game designed to test your speed against the clock!",
-//         descr2: "Built in Javascript with React, Firebase and tsParticles, Number Blast offers a fun and frantic gaming experience to challenge you and your freinds! How fast are you under pressure?",
-//         descr3Bold: "States of Play.",
-//         descr3: "Built in React with heavy use of state, effect and callback hooks to handle evolving game, level, timer and styling changes.",
-//         descr4Bold: "Global Leaderboard.",
-//         descr4: "What's wrong with a friendly competition? Maybe a lot when I can't even crack the top five of my own game any more! Do you have what it takes to reach the top spot?",
-//         descr5Bold: "Interactive Styling.",
-//         descr5: "Those background numbers are not just for show. Using tsParticles, they are fully interactive and even add to the frantic pace of higher game levels. Have you got what it takes to remain calm?",
-//     },
-// ]
 
 export default function Project ({projects}) {
 
@@ -32,16 +19,23 @@ export default function Project ({projects}) {
             <p>{descr1}</p>
             <p>{descr11}</p>
             <p>{descr2}</p>
-            {github === "" ? null : <a href={github} target="blank">Find the repository on </a>}
-            <a href={link} target="blank">{linkDescr} </a>
-            {link2 === "" ? null : <a href={link2} target="blank">{link2Descr} </a>}
-            {link3 === "" ? null : <a href={link3} target="blank">{link3Descr} </a>}
-            {link4 === "" ? null : <a href={link4} target="blank">{link4Descr} </a>}
+            {github === "" ? null : <a href={github} target="blank"><button>Find the repository on </button></a>}
+            <a href={link} target="blank"><button>{linkDescr}</button> </a>
+            {link2 === "" ? null : <a href={link2} target="blank"><button>{link2Descr}</button> </a>}
+            {link3 === "" ? null : <a href={link3} target="blank"><button>{link3Descr}</button> </a>}
+            {link4 === "" ? null : <a href={link4} target="blank"><button>{link4Descr}</button> </a>}
             {descr3 === "" ? null : <p><strong>{descr3Bold}</strong>{" " +descr3}</p>}
             {descr4 === "" ? null : <p><strong>{descr4Bold}</strong>{" " +descr4}</p>}
             {descr5 === "" ? null : <p><strong>{descr5Bold}</strong>{" " +descr5}</p>}
             <a href={link4} target="blank">{link4Descr} </a>
 
+                <div>
+                        <Link to="/home">
+                            <button>
+                                <p>Back</p>
+                            </button>   
+                        </Link>
+                    </div>
         
         </>
     )
