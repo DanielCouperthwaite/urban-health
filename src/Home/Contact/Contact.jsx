757930449e00b6ab9220e6a5aa01e8e7f0fcf395
DanielCouperthwaite <danielcoup@hotmail.co.uk>
2024-06-23 23:16:45 +0100

    submission form styling

--- src/Home/Contact/Contact.jsx
+++ src/Home/Contact/Contact.jsx
@@ -6,6 +6,10 @@ import { Box, Grow } from '@mui/material'
 import face from "../../assets/face.png"
 import ParticleEffectAbout from '../About/ParticleEffectAbout'
 
+import fb from "../../assets/facebook-logo.png"
+import ig from "../../assets/instagram-logo.png"
+import tw from "../../assets/twitter-logo.png"
+
 
 export default function Contact () {
 
@@ -46,17 +50,17 @@ export default function Contact () {
 
                 })
             })
-                .then(response => response.json())
-                .then(data => console.log(data))
-                .then(() => {
-                    setEmail("")
-                    setMessage("")
-                    setName("")
-                    setSubmitted(false)
-                })
-                .then(() => {
-                    alert("Message Sent!")
-                })
+                // .then(response => response.json())
+                // .then(data => console.log(data))
+                // .then(() => {
+                //     setEmail("")
+                //     setMessage("")
+                //     setName("")
+                //     setSubmitted(false)
+                // })
+                // .then(() => {
+                //     alert("Message Sent!")
+                // })
                 .catch((error) => alert("Unfortunately, there was a problem with your submission. Please try again!"));
 
 
@@ -69,7 +73,7 @@ export default function Contact () {
                     name="contact"
                     onSubmit={handleSubmit}
                 >
-                    <h2 >Contact</h2>
+                    
                     <div>
                         <label htmlFor="name">Name</label>
                         <input
@@ -80,13 +84,13 @@ export default function Contact () {
 
                             onChange={(event) => setName(event.target.value)}
                         />
-
                     </div>
-                    <div >
+                    
+                    <div>
                         <label htmlFor="email">
                             Email</label>
 
-                        <input
+                            <input
                             type="email"
                             id="email"
                             name="email"
@@ -107,7 +111,7 @@ export default function Contact () {
                         />
 
                     </div>
-                    <button type="submit">Submit</button>
+                    <button className='submit' type="submit">Submit</button>
                 </form>
             </>
         )
@@ -143,19 +147,39 @@ export default function Contact () {
                                         </div>  
                     
                         
-                        <h3 style={{marginTop: "-20px"}}>Contact</h3>
-                        <p>See below for details on where to contact us, or fill in a form and we will get back to you as soon as possible!</p>
+                        <h3 style={{marginTop: "-20px"}}>Get in Touch!</h3>
+                        <p>Follow us on social media, or fill in a form and we will get back to you as soon as possible!</p>
+
+                        <div className='social-contact'>
+                                            <a  href="https://www.facebook.com/share/JNqew8PHdicTvR1N/?mibextid=LQQJ4d" target='_blank'>
+                                                <button className="social-link">
+                                                    <img src={fb} alt="Facebook logo" style={{width: "80%", height: "auto"}}></img>
+                                                </button>
+                                            </a>
+                                            <a href="https://www.instagram.com/urbanhealthuk2024?igsh=MWN2MGVtcTFvdW01MQ%3D%3D&utm_source=qr" target='_blank'>
+                                                <button className="social-link">
+                                                    <img src={ig} alt="Instagram Logo" style={{width: "80%", height: "auto"}}></img>
+                                                </button>
+                                            </a>
+                                            <a href="https://twitter.com/UrbanHealthUK24" target='_blank'>
+                                                <button className="social-link">
+                                                    <img src={tw} alt="Twitter Logo" style={{width: "80%", height: "auto"}}></img>
+                                                </button>
+                                            </a>
+                                        </div>  
+
+
                     </div>
                     </Grow>    
                     <Grow in={loadTwo}>
                     <div className="about">
 
 
-                    {submitted === false ? <Form /> : <p>Message sent! Thanks for getting in touch, we will reply as soon as we can.</p>}
+                    {submitted === false ? <Form /> : <div className='submitted'><p>Thanks for getting in touch!<br /><br /> We will reply as soon as we can.</p></div>}
 
 
 
-                        <div>
+                        {/* <div>
                             <div style={{textAlign: "center", marginBottom: "80px"}}>
                                         <Link target="blank" to="https://emea01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fform.jotform.com%2F241347125439052&data=05%7C02%7C%7C2e6fbbd2d57342fee1fe08dc8bbcc227%7C84df9e7fe9f640afb435aaaaaaaaaaaa%7C1%7C0%7C638538890033979462%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C0%7C%7C%7C&sdata=hfgNwuPjpt9ODcEcLUFTDorikzqgQnoIvugrdoDSROE%3D&reserved=0">
                                             <button style={{width: "500px"}}>
@@ -163,8 +187,8 @@ export default function Contact () {
                                             </button>   
                                         </Link>
                             </div>
-                        </div>
-                        <div className='tech-info'>
+                        </div> */}
+                        {/* <div className='tech-info'>
                             <p className='tech-span'><strong>Email: </strong></p>
                             <p className='tech-p'>example@email.com</p>
                             <p className='tech-span'><strong>Phone:</strong></p>
@@ -173,7 +197,7 @@ export default function Contact () {
                             <p className='tech-p'>www.donations-example.com</p>
                             <p className='tech-span'><strong>Find us on:</strong></p>
                             <p className='tech-p'>Social Media Links Here</p>
-                        </div>
+                        </div> */}
                         
                         <div>
                             <div style={{textAlign: "center"}}>
